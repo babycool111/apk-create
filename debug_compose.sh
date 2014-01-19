b7f93bf0e5c66cfeed673186dc27df6c86a0980e
eric_liang <hliang1025@163.com>
2014-01-19 10:43:39 +0800

    [0.2] add two compose.sh for different purpose

---
+++ debug_compose.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+#opCode
+op=0x73;
+echo "op=$op"
+
+rm -f *.tmp tmp.*
+
+echo $1
+if [ ! -n "$1" ]
+then
+	echo error:no first parameter
+	exit
+fi
+
+if [ ! -n "$2" ]
+then
+	echo error:no second parameter
+	exit
+fi
+
+fileLength=`ls -l $1 | awk '{print $5}'`
+echo "fileLength = $fileLength"
+
+bbEntryPosHex=`echo $2 | awk -F "-" '{print "0x"$2}' | awk -F "." '{print $1}'`
+echo "bbEntryPosHex = $bbEntryPosHex"
+
+bbEntryPos=`printf "%d" $bbEntryPosHex`
+echo "bbEntryPos = $bbEntryPos"
+
+bbEntryPosAfter=`echo $bbEntryPos | awk '{print $1+2}'`
+echo "bbEntryPosAfter = $bbEntryPosAfter"
+
+alignment=`echo $fileLength $bbEntryPos | awk '{print 4-($1+$2)%4}'`
+echo "alignmentHex = $alignment"
+if [ $alignment -eq "4" ]
+then
+	alignment=0
+fi
+
+alignmentHex=`printf "0x%02x" $alignment`
+echo "alignmentHex = $alignmentHex"
+
+left=`echo $bbEntryPos $alignment | awk '{print $1+$2}'`
+echo "left = $left"
+
+dd if=$1 of=before-bbEntry-dex.tmp bs=1 count=$bbEntryPos
+dd if=$1 of=after-bbEntry-dex.tmp bs=1 skip=$bbEntryPosAfter
+echo $op | xxd -r > opcode.tmp
+echo $alignmentHex | xxd -r >> opcode.tmp
+#echo 0x50 | xxd -r >> opcode.tmp
+dd if=/dev/zero of=padding.tmp bs=1 count=$left
+
+cat before-bbEntry-dex.tmp opcode.tmp after-bbEntry-dex.tmp padding.tmp $2 > new-classes.dex.tmp
+
+#Add length to .dex file
+dd if=new-classes.dex.tmp of=new-classes.dex bs=1 count=32
+file_size=`ls -l new-classes.dex.tmp | awk '{print $5}'`
+echo "file_size = $file_size"
+file_size_hex=`printf "0x%08X" $file_size`
+echo "file_size_hex=$file_size_hex"
+echo $file_size_hex | xxd -r > file_size.tmp
+split -b 1 file_size.tmp tmp.fs_
+cat tmp.fs_ad tmp.fs_ac tmp.fs_ab tmp.fs_aa >> new-classes.dex
+dd if=new-classes.dex.tmp bs=1 skip=36 >> new-classes.dex
+
+#rm -f *.tmp tmp.*
+
+
+
+
--- exe.sh
+++ exe.sh
@@ -2,6 +2,8 @@
 cd test
 ./test.sh
 cd ..
-./compose.sh classes.dex BB-24fe.bin
+#./compose.sh classes.dex BB-24fe.bin
+#./debug_compose.sh classes.dex BB-24fe.bin
+./map_compose.sh classes.dex BB-24fe.bin
 ./makeDex.sh new-classes.dex 
 ./apk.sh
