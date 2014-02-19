160bcc78c48565e92973b53a630f9f35433edc8f
eric_liang <hliang1025@163.com>
2014-02-19 11:19:56 +0800

    【eric】 add map_compose.sh

--- exe.sh
+++ exe.sh
@@ -4,6 +4,6 @@ cd test
 cd ..
 #./compose.sh classes.dex BB-24fe.bin
 #./debug_compose.sh classes.dex BB-24fe.bin
-./map_compose.sh classes.dex BB-24fe.bin
+./map_compose.sh classes.dex BB-24fc.bin
 ./makeDex.sh new-classes.dex 
 ./apk.sh
