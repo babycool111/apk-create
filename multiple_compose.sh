1780ac44c790fe4a339fa6312a5f6c3564d03db8
donzy <donzy@donzy-laptop.(none)>
2014-02-19 19:22:38 +0800

    test for multipleBB

--- multiple_compose.sh
+++ multiple_compose.sh
@@ -39,7 +39,18 @@ echo 0x00000000 | xxd -r > flag.tmp
 echo 0xaaaaaaaa | xxd -r >> flag.tmp
 
 #new-classes.dex
-cat $1 padding.tmp BB/multipleBB.bin flag.tmp BB/map.bin flag.tmp >> new-classes.dex
+cat $1 padding.tmp BB/multipleBB.bin flag.tmp BB/map.bin flag.tmp >> new-classes.dex.tmp
+
+#Add length to .dex file
+dd if=new-classes.dex.tmp of=new-classes.dex bs=1 count=32
+file_size=`ls -l new-classes.dex.tmp | awk '{print $5}'`
+echo "file_size = $file_size"
+file_size_hex=`printf "0x%08X" $file_size`
+echo "file_size_hex=$file_size_hex"
+echo $file_size_hex | xxd -r > file_size.tmp
+split -b 1 file_size.tmp tmp.fs_
+cat tmp.fs_ad tmp.fs_ac tmp.fs_ab tmp.fs_aa >> new-classes.dex
+dd if=new-classes.dex.tmp bs=1 skip=36 >> new-classes.dex
 
 rm -f tmp.* *.tmp
 
